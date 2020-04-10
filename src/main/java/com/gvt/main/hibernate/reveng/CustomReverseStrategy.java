6c1f280af5523a26514dee29509d87eaa8dcf594
harold <harold@dev01>
2020-04-10 17:24:02 +0200

    Fix format and imports

--- src/main/java/com/gvt/main/hibernate/reveng/CustomReverseStrategy.java
+++ src/main/java/com/gvt/main/hibernate/reveng/CustomReverseStrategy.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
-import org.apache.commons.lang.StringUtils;
 import org.hibernate.cfg.reveng.DelegatingReverseEngineeringStrategy;
 import org.hibernate.cfg.reveng.ReverseEngineeringStrategy;
 import org.hibernate.cfg.reveng.TableIdentifier;
